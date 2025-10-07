6f0907470cf34bf75f19b73e2eecfe2cce707a1c
tippett33 <151669674+tippett33@users.noreply.github.com>
2025-10-07 19:28:05 +0000

    Deploying to gh-pages from @ tippett33/tippett33.github.io@9a04b0964095b929737da249544c8e40228c66c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-about",
-          title: "about",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "A collection of some of my most interesting projects.",
           section: "Navigation",
