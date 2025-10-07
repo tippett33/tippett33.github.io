c6eee96b32a4d434ae28207b96a4cf8b5930e175
tippett33 <151669674+tippett33@users.noreply.github.com>
2025-10-07 17:38:00 +0000

    Deploying to gh-pages from @ tippett33/tippett33.github.io@e7420b7380c090294b81d96bda3472472055896c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/about/";
+            window.location.href = "/";
           },
         },{id: "nav-publications",
           title: "publications",
