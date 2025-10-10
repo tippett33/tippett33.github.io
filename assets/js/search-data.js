3b884b5cba88b171951a443e44fb7b8b9d0b68ca
tippett33 <151669674+tippett33@users.noreply.github.com>
2025-10-10 08:32:37 +0000

    Deploying to gh-pages from @ tippett33/tippett33.github.io@306d4b8e3aeccf3de622651318b966fb3c62ec69 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -400,8 +400,8 @@ ninja.data = [{
           description: "Key literature for EPOG meeting Barcelona 2025",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-epog-literature-topic-model",
+          title: 'EPOG literature topic model',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
